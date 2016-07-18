f10682ac6975303b8be5bb5058c7f4a5f9d15640
vishaldhaduk11 <vishal.dhaduk@lavasoft.com>
2016-07-18 14:43:41 -0400

    web app created for MVC examples

---
+++ MVCSamples/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCSamples.MvcApplication" Language="C#" %>
---
+++ MVCSamples/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCSamples.Startup))]
+namespace MVCSamples
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
