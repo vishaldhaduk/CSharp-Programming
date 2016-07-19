1507ef33f0aacb6568c69f884a506fa8119f6f16
vishaldhaduk <vishal.dhaduk11@gmail.com>
2016-07-19 09:37:30 -0400

    New MVC application created for tutorials

---
+++ MVCApplication/Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCApplication.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string GetString()
+        {
+            return "Hello World is old now. It&rsquo;s time for wassup bro ;)";
+        }
+
+        public ActionResult GetView()
+        {
+            return View("MyView");
+        }
+    }
+}
